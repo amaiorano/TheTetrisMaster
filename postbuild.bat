c212f0bbf1979464b2a0e0cb9e80d0fed5b7d2b9
Antonio Maiorano <amaiorano@gmail.com>
2014-08-06 19:59:44 -0400

    Simplify postbuild script by making all projects build to the same _output folder

--- postbuild.bat
+++ postbuild.bat
@@ -1,13 +1,8 @@
 @echo off
 
 echo # Running post-build script...
-
-rem *** Input param 1 is current directory
 echo # Current directory is %1
-
-rem *** Input param 2 is configuration (debug or release)
 echo # Current configuration is %2
-
 
 rem *** Set local variables
 setlocal
@@ -17,56 +12,20 @@ if "%CFG%"=="" goto :ERROR
 
 set COPY_CMD=xcopy.exe /S /R /K /Y /D
 
-set DATA_DIR=.\Data
-set OUTPUT_DIR=".\output\The Tetris Master"
-set THEME_DIR=%OUTPUT_DIR%\Themes
+set SRC_DATA_DIR=.\Data
+set SRC_EXE_DIR=.\_output\%CFG%
+set OUTPUT_DIR=".\_game\The Tetris Master\"
 
-set TETRIS_EXE=".\Tetris\%CFG%\Tetris.exe"
-set LAUNCHER_EXE=".\ThemeLauncher\%CFG%\TTM Theme Launcher.exe"
-set EDITOR_EXE=".\ThemeEditor\%CFG%\TTM Theme Editor.exe"
-set AUDIERE_DLL=".\GSGameLib\gsgamelib\external\audiere\audiere.dll"
+set SRC_FILES=("%SRC_EXE_DIR%\Tetris\Tetris.exe" "%SRC_EXE_DIR%\ThemeLauncher\TTM Theme Launcher.exe" "%SRC_EXE_DIR%\ThemeEditor\TTM Theme Editor.exe" ".\GSGameLib\gsgamelib\external\audiere\audiere.dll" ".\Tetris\ReadMe.txt")
 
-set README_TXT=".\Tetris\ReadMe.txt"
-
-rem *** Store actual current directory go to input one
 pushd %1
 
-echo # Creating output directory: %OUTPUT_DIR%
-if not exist %OUTPUT_DIR% md %OUTPUT_DIR%
-
-rem *** Always copy themes
-goto :COPY_DATA
-echo # Copying data to output directory
-rem *** Only copy data if folders not present in output directory
-if not exist %THEME_DIR% goto :COPY_DATA
-echo # Data already exists in output directory, skipping
-goto :END_COPY_DATA
-
 :COPY_DATA
-if not exist %THEME_DIR% md %THEME_DIR%
-%COPY_CMD% %DATA_DIR% %OUTPUT_DIR%
+%COPY_CMD% %SRC_DATA_DIR% %OUTPUT_DIR%
 if not "%errorlevel%"=="0" goto :ERROR
-:END_COPY_DATA
 
 echo # Copying executables and dlls
-rem *** Because VS.NET does not have an option to ALWAYS run custom build-events,
-rem *** we have to run this script for each project. So don't check for errors
-rem *** because some of these will fail depending on the order the script is called.
-%COPY_CMD% %TETRIS_EXE% %OUTPUT_DIR%
-rem	if not "%errorlevel%"=="0" goto :ERROR
-
-%COPY_CMD% %LAUNCHER_EXE% %OUTPUT_DIR%
-rem	if not "%errorlevel%"=="0" goto :ERROR
-
-%COPY_CMD% %EDITOR_EXE% %OUTPUT_DIR%
-rem	if not "%errorlevel%"=="0" goto :ERROR
-
-%COPY_CMD% %AUDIERE_DLL% %OUTPUT_DIR%
-if not "%errorlevel%"=="0" goto :ERROR
-
-echo # Copying ReadMe file
-%COPY_CMD% %README_TXT% %OUTPUT_DIR%
-if not "%errorlevel%"=="0" goto :ERROR
+for %%i in %SRC_FILES% do %COPY_CMD% %%i %OUTPUT_DIR%
 
 goto :END
 :ERROR
@@ -75,6 +34,5 @@ echo # Error encountered, bailing...
 :END
 echo # Post-build script ended with errorlevel %errorlevel%
 
-rem *** Restore original directory
 popd
 @echo on
