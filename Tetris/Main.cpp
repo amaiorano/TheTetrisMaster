9bba4a7b6a2c12dae43a3c54bf6436d38ce604f3
Antonio Maiorano <amaiorano@gmail.com>
2014-08-06 00:00:04 -0400

    Simplify main and fix default theme not loading when exe path has spaces in it

--- Tetris/Main.cpp
+++ Tetris/Main.cpp
@@ -4,44 +4,12 @@
 #include <vector>
 using namespace std;
 
-// Comment out when not profiling
-#define PROFILING
-
-// Comment out when using Windows subsystem, otherwise keep it for Console
-/* #define CONSOLE_APP */
-
-vector<string> argList; // Will contain command line arguments
-
-#ifdef CONSOLE_APP
-int main(int argc, char* argv[])
-{
-	for (int i=0; i<argc; ++i)
-		argList.push_back(argv[i]);
-
-#else
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
 {
-	// Extract command line parameters
-	stringstream cmdLineStream(GetCommandLine());
-	string strTemp;
-	cmdLineStream >> strTemp; 
-	argList.push_back(strTemp); // First param: program name
-	argList.push_back(cmdLineStream.str()); // Second param: theme path
-
-#endif
-
-	// Ignore input argument in profiling mode, we default to "Guardian Soft" theme
-	// Reason: Profiler bug fails to spawn app if arguments specified in project
-#ifndef PROFILING
-	if ( argList.size() != 2 )
-	{
-		cerr << "Usage (example): " << argList[0] << " .\\themes\\default\\default.the" << endl;
-		return -1;
-	}
-#endif
-
 	// Set theme path - if none supplied, then use default
-	string strThemePath = ( argList.size()>1? argList[1] : "Guardian Soft");
+	string strThemePath = lpCmdLine;
+	if (strThemePath.empty())
+		strThemePath = "Guardian Soft\\Guardian Soft.the";
 
 	// Extract theme name from path
 	string::size_type extPos =			strThemePath.rfind(".");
